0a20a99bf1404d5de4bf5dee49da82fce2f2aa2c
admin <admin@admin.com>
2026-07-24 01:07:22 +0900

    commit

--- 영희.py
+++ 영희.py
@@ -1,6 +1,5 @@
 print(1)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 print(2)
 print(3)
 print(2)
@@ -11,10 +10,6 @@ print(1)
 print(2)
 print(3)
 print(2)
-
-=======
->>>>>>> 367f320 (commit)
-=======
 print(2)
->>>>>>> ddc3ce4 (commit)
+
 
